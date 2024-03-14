52899ff1bb651515a4403615c53974c84e6ce1e3
Sandro Junior <sandrojr998@gmail.com>
2024-03-14 17:02:10 -0300

    final

--- api/cad.php
+++ api/cad.php
@@ -6,21 +6,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <h1>LOGIN</h1>
-    <form action="action-login.php" method="post">
+    <h1>CADASTRAR USUARIO</h1>
+    <form action="action-users.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="name" required>
         <label for="email">Email:</label>
         <input type="email" name="email" id="email" required>
         <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha" required>
+        <input type="text" name="senha" id="senha" required>
         <button type="submit">Enviar</button>
     </form>
-    <a href="../final.php">Novo usuario</a>
-
-
 </body>
 
 </html>
